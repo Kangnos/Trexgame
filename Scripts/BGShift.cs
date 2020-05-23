87ddc01b35fbdf62ff36904caa682e913cd2b7b6
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-05-23 10:47:00 +0900

    Add files via upload

---
+++ Scripts/BGShift.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGShift : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit2D(Collider2D collider){
+        float width=((BoxCollider2D)collider).size.x; // 가로길이 
+        Vector3 pos = collider.transform.position;
+        pos.x += width* 1.6f; // 만약 그것과 만나면 위치를 바꾼다.
+        if(collider.gameObject.tag == "BG1"){
+            collider.transform.position = pos;
+        }
+        if(collider.gameObject.tag == "BG2"){
+            collider.transform.position = pos;
+        }
+    }
+}
