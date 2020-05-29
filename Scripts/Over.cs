557f2ae95654ef0ad3e19c0987355235b203b5b0
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-05-29 14:58:50 +0900

    Add files via upload

--- Scripts/Over.cs
+++ Scripts/Over.cs
@@ -1,23 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Over : MonoBehaviour
 {
+    public bool over;
+    public GameObject GameoverImage;
+    public GameObject restartButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        over = false;
+        GameoverImage.SetActive(false);
+        restartButton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.name == "CloneMaker"){
             Time.timeScale = 0;
+            over = true;
+        }
+    }
+    void OnGUI(){
+        if(over){
+            GameoverImage.SetActive(true);
+            restartButton.SetActive(true);
+            if(Input.GetMouseButtonDown(0) || Input.GetKeyDown("r")){
+                Application.LoadLevel("TREX-GAME");
+                Time.timeScale = 1;
+                
+            }
         }
     }
 }
